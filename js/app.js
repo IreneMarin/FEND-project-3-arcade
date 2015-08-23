dc5a97209dbc3b9c22180d94a259ed445c84d9b6
IreneMarin <irene@marin.cat>
2015-08-23 21:02:26 +0200

    Gitignore and uploading images

--- js/app.js
+++ js/app.js
@@ -24,12 +24,16 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
+var player = function() {
+   
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [
+    
+];
 
 
 // This listens for key presses and sends the keys to your
