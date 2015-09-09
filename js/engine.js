cfe5a4ee5d2d645634abb75859267260751bd27f
IreneMarin <irene@marin.cat>
2015-09-09 18:01:53 +0200

    reviewer optimizations

--- js/engine.js
+++ js/engine.js
@@ -1,23 +1,23 @@
-/* Engine.js
- * This file provides the game loop functionality (update entities and render),
- * draws the initial game board on the screen, and then calls the update and
- * render methods on your player and enemy objects (defined in your app.js).
+/** Engine.js
+ *  This file provides the game loop functionality (update entities and render),
+ *  draws the initial game board on the screen, and then calls the update and
+ *  render methods on your player and enemy objects (defined in your app.js).
  *
- * A game engine works by drawing the entire game screen over and over, kind of
- * like a flipbook you may have created as a kid. When your player moves across
- * the screen, it may look like just that image/character is moving or being
- * drawn but that is not the case. What's really happening is the entire "scene"
- * is being drawn over and over, presenting the illusion of animation.
+ *  A game engine works by drawing the entire game screen over and over, kind of
+ *  like a flipbook you may have created as a kid. When your player moves across
+ *  the screen, it may look like just that image/character is moving or being
+ *  drawn but that is not the case. What's really happening is the entire "scene"
+ *  is being drawn over and over, presenting the illusion of animation.
  *
- * This engine is available globally via the Engine variable and it also makes
- * the canvas' context (ctx) object globally available to make writing app.js
- * a little simpler to work with.
+ *  This engine is available globally via the Engine variable and it also makes
+ *  the canvas' context (ctx) object globally available to make writing app.js
+ *  a little simpler to work with.
  */
 
 var Engine = (function(global) {
-    /* Predefine the variables we'll be using within this scope,
-     * create the canvas element, grab the 2D context for that canvas
-     * set the canvas elements height/width and add it to the DOM.
+    /** Predefine the variables we'll be using within this scope,
+     *  create the canvas element, grab the 2D context for that canvas
+     *  set the canvas elements height/width and add it to the DOM.
      */
     var doc = global.document,
         win = global.window,
@@ -25,57 +25,56 @@ var Engine = (function(global) {
         ctx = canvas.getContext('2d'),
         lastTime;
 
-    // Create the size of the canvas
+    /** Create the size of the canvas */
     canvas.width = 909;
     canvas.height = 760;
     doc.body.appendChild(canvas);
 
-    /* This function serves as the kickoff point for the game loop itself
-     * and handles properly calling the update and render methods.
+    /** This function serves as the kickoff point for the game loop itself
+     *  and handles properly calling the update and render methods.
      */
     function main() {
-        /* Get our time delta information which is required if your game
-         * requires smooth animation. Because everyone's computer processes
-         * instructions at different speeds we need a constant value that
-         * would be the same for everyone (regardless of how fast their
-         * computer is) - hurray time!
+        /** Get our time delta information which is required if your game
+         *  requires smooth animation. Because everyone's computer processes
+         *  instructions at different speeds we need a constant value that
+         *  would be the same for everyone (regardless of how fast their
+         *  computer is) - hurray time!
          */
         var now = Date.now(),
             dt = (now - lastTime) / 1000.0;
 
-        /* Call our update/render functions, pass along the time delta to
-         * our update function since it may be used for smooth animation.
+        /** Call our update/render functions, pass along the time delta to
+         *  our update function since it may be used for smooth animation.
          */
         update(dt);
         render();
 
-        /* Set our lastTime variable which is used to determine the time delta
-         * for the next time this function is called.
+        /** Set our lastTime variable which is used to determine the time delta
+         *  for the next time this function is called.
          */
         lastTime = now;
 
-        /* Use the browser's requestAnimationFrame function to call this
-         * function again as soon as the browser is able to draw another frame.
+        /** Use the browser's requestAnimationFrame function to call this
+         *  function again as soon as the browser is able to draw another frame.
          */
-         
         win.requestAnimationFrame(main);
     };
 
-    /* This function does some initial setup that should only occur once,
-     * particularly setting the lastTime variable that is required for the
-     * game loop.
+    /** This function does some initial setup that should only occur once,
+     *  particularly setting the lastTime variable that is required for the
+     *  game loop.
      */
     function init() {
         lastTime = Date.now();
         main();
     }
 
-    /* This function is called by main (our game loop) and itself calls all
-     * of the functions which may need to update entity's data. 
+    /** This function is called by main (our game loop) and itself calls all
+     *  of the functions which may need to update entity's data. 
      */
     function update(dt) {
-        switch(CURRENT_LEVEL) {
-            // If we are in the starting screen, we don't show the canvas
+        switch(currentLevel) {
+            /** If we are in the starting screen, we don't show the canvas */
             case 0:
                 break;
         
@@ -84,20 +83,20 @@ var Engine = (function(global) {
                 break;
         }
         
-        /* There is a bug when rendering the game and the Player 
-         * arrives to the top of the screen, the head of the player 
-         * stays rendered behind the tiles.
-         * Thanks to Sebastian in the forums for this fix.
+        /** There is a bug when rendering the game and the Player 
+         *  arrives to the top of the screen, the head of the player 
+         *  stays rendered behind the tiles.
+         *  Thanks to Sebastian in the forums for this fix.
          */
         ctx.clearRect(0,0,canvas.width,canvas.height);
     }
 
-    /* This is called by the update function  and loops through all of the
-     * objects within your allEnemies array as defined in app.js and calls
-     * their update() methods. It will then call the update function for your
-     * player object. These update methods should focus purely on updating
-     * the data/properties related to  the object. Do your drawing in your
-     * render methods.
+    /** This is called by the update function  and loops through all of the
+     *  objects within your allEnemies array as defined in app.js and calls
+     *  their update() methods. It will then call the update function for your
+     *  player object. These update methods should focus purely on updating
+     *  the data/properties related to  the object. Do your drawing in your
+     *  render methods.
      */
     function updateEntities(dt) {
         allEnemies.forEach(function(enemy) {
@@ -106,16 +105,16 @@ var Engine = (function(global) {
         player.update();
     }
     
-    /* This function initially draws the "game level", it will then call
-     * the renderEntities function. Remember, this function is called every
-     * game tick (or loop of the game engine) because that's how games work -
-     * they are flipbooks creating the illusion of animation but in reality
-     * they are just drawing the entire screen over and over.
+    /** This function initially draws the "game level", it will then call
+     *  the renderEntities function. Remember, this function is called every
+     *  game tick (or loop of the game engine) because that's how games work -
+     *  they are flipbooks creating the illusion of animation but in reality
+     *  they are just drawing the entire screen over and over.
      */
     function render() {
-        /* First we create the variables of the different types of ground we have.
-         * Afterwards we create a matrix for each row/column, where we will put
-         * each type of ground for each spot, that will vary for each level.
+        /** First we create the variables of the different types of ground we have.
+         *  Afterwards we create a matrix for each row/column, where we will put
+         *  each type of ground for each spot, that will vary for each level.
          */
         var s = 'images/stone-block.png';
         var g = 'images/grass-block.png';
@@ -124,9 +123,9 @@ var Engine = (function(global) {
         var f = 'images/wood-block.png';
         var matrix;
          
-        // TODO: find a better way to store and access the matrix for the grounds
+        /** TODO: find a better way to store and access the matrix for the grounds */
                
-        switch(CURRENT_LEVEL)
+        switch(currentLevel)
         {
             case 0:
                 document.getElementById('menu').hidden = true;
@@ -147,21 +146,20 @@ var Engine = (function(global) {
                     [s,s,s,s,s,s,s,s,s]  // row 8
                 ];
                 
-                /* Loop through the number of rows and columns we've defined above
-                * and, using the matrix double array, draw the correct image for that
-                * portion of the "grid"
-                */
+                /** Loop through the number of rows and columns we've defined above
+                 *  and, using the matrix double array, draw the correct image for that
+                 *  portion of the "grid"
+                 */
                 for (var row = 0; row < 8; row++) {
                     for (var col = 0; col < 9; col++) {
-                        /* The drawImage function of the canvas' context element
-                        * requires 3 parameters: the image to draw, the x coordinate
-                        * to start drawing and the y coordinate to start drawing.
-                        * We're using our Resources helpers to refer to our images
-                        * so that we get the benefits of caching these images, since
-                        * we're using them over and over.
-                        */
+                        /** The drawImage function of the canvas' context element
+                         *  requires 3 parameters: the image to draw, the x coordinate
+                         *  to start drawing and the y coordinate to start drawing.
+                         *  We're using our Resources helpers to refer to our images
+                         *  so that we get the benefits of caching these images, since
+                         *  we're using them over and over.
+                         */
                         ctx.drawImage(Resources.get(matrix[row][col]), col * 101, row * 83);
-                        //console.log(rowMatriu[row][col]);
                     }
                 }
                 renderEntities();
@@ -182,18 +180,18 @@ var Engine = (function(global) {
                 ];
                 
                 /* Loop through the number of rows and columns we've defined above
-                * and, using the matrix double array, draw the correct image for that
-                * portion of the "grid"
-                */
+                 * and, using the matrix double array, draw the correct image for that
+                 * portion of the "grid"
+                 */
                 for (var row = 0; row < 8; row++) {
                     for (var col = 0; col < 9; col++) {
                         /* The drawImage function of the canvas' context element
-                        * requires 3 parameters: the image to draw, the x coordinate
-                        * to start drawing and the y coordinate to start drawing.
-                        * We're using our Resources helpers to refer to our images
-                        * so that we get the benefits of caching these images, since
-                        * we're using them over and over.
-                        */
+                         * requires 3 parameters: the image to draw, the x coordinate
+                         * to start drawing and the y coordinate to start drawing.
+                         * We're using our Resources helpers to refer to our images
+                         * so that we get the benefits of caching these images, since
+                         * we're using them over and over.
+                         */
                         ctx.drawImage(Resources.get(matrix[row][col]), col * 101, row * 83);
                     }
                 }
@@ -215,18 +213,18 @@ var Engine = (function(global) {
                 ];
                 
                 /* Loop through the number of rows and columns we've defined above
-                * and, using the matrix double array, draw the correct image for that
-                * portion of the "grid"
-                */
+                 * and, using the matrix double array, draw the correct image for that
+                 * portion of the "grid"
+                 */
                 for (var row = 0; row < 8; row++) {
                     for (var col = 0; col < 9; col++) {
                         /* The drawImage function of the canvas' context element
-                        * requires 3 parameters: the image to draw, the x coordinate
-                        * to start drawing and the y coordinate to start drawing.
-                        * We're using our Resources helpers to refer to our images
-                        * so that we get the benefits of caching these images, since
-                        * we're using them over and over.
-                        */
+                         * requires 3 parameters: the image to draw, the x coordinate
+                         * to start drawing and the y coordinate to start drawing.
+                         * We're using our Resources helpers to refer to our images
+                         * so that we get the benefits of caching these images, since
+                         * we're using them over and over.
+                         */
                         ctx.drawImage(Resources.get(matrix[row][col]), col * 101, row * 83);
                     }
                 }
@@ -248,18 +246,18 @@ var Engine = (function(global) {
                 ];
                 
                 /* Loop through the number of rows and columns we've defined above
-                * and, using the matrix double array, draw the correct image for that
-                * portion of the "grid"
-                */
+                 * and, using the matrix double array, draw the correct image for that
+                 * portion of the "grid"
+                 */
                 for (var row = 0; row < 8; row++) {
                     for (var col = 0; col < 9; col++) {
                         /* The drawImage function of the canvas' context element
-                        * requires 3 parameters: the image to draw, the x coordinate
-                        * to start drawing and the y coordinate to start drawing.
-                        * We're using our Resources helpers to refer to our images
-                        * so that we get the benefits of caching these images, since
-                        * we're using them over and over.
-                        */
+                         * requires 3 parameters: the image to draw, the x coordinate
+                         * to start drawing and the y coordinate to start drawing.
+                         * We're using our Resources helpers to refer to our images
+                         * so that we get the benefits of caching these images, since
+                         * we're using them over and over.
+                         */
                         ctx.drawImage(Resources.get(matrix[row][col]), col * 101, row * 83);
                     }
                 }
@@ -267,7 +265,7 @@ var Engine = (function(global) {
                 break;
         }
         
-        // Function to wrap the text that will appear on the canvas when game_over or game_final
+        /** Function to wrap the text that will appear on the canvas when game_over or game_final */
         function wrapText(ctx, text, x, y, maxWidth, lineHeight) {
             var words = text.split(' ');
             var line = '';
@@ -316,13 +314,13 @@ var Engine = (function(global) {
         }
     }
 
-    /* This function is called by the render function and is called on each game
-     * tick. It's purpose is to then call the render functions you have defined
-     * on your enemy and player entities within app.js
+    /** This function is called by the render function and is called on each game
+     *  tick. It's purpose is to then call the render functions you have defined
+     *  on your enemy and player entities within app.js
      */
     function renderEntities() {
-        /* Loop through all of the objects within the allEnemies array and call
-         * the render function you have defined.
+        /** Loop through all of the objects within the allEnemies array and call
+         *  the render function you have defined.
          */
         allObstacles.forEach(function(obstacle){
            obstacle.render(); 
@@ -340,9 +338,9 @@ var Engine = (function(global) {
     }
     
     
-    /* Go ahead and load all of the images we know we're going to need to
-     * draw our game level. Then set init as the callback method, so that when
-     * all of these images are properly loaded our game will start.
+    /** Go ahead and load all of the images we know we're going to need to
+     *  draw our game level. Then set init as the callback method, so that when
+     *  all of these images are properly loaded our game will start.
      */
     Resources.load([
         'images/stone-block.png',
@@ -386,9 +384,9 @@ var Engine = (function(global) {
     ]);
     Resources.onReady(init);
 
-    /* Assign the canvas' context object to the global variable (the window
-     * object when run in a browser) so that developer's can use it more easily
-     * from within their app.js files.
+    /** Assign the canvas' context object to the global variable (the window
+     *  object when run in a browser) so that developer's can use it more easily
+     *  from within their app.js files.
      */
     global.ctx = ctx;
 })(this);
